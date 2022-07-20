3b6eb838365a0d5057bd6b4f918ec529b66941ae
PriyankaNandi <priyanka.vnandi@gmail.com>
2022-07-20 16:18:16 +0530

    setup
    app setup

---
+++ windows/coin_wealth/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::coin_wealth::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::coin_wealth::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
